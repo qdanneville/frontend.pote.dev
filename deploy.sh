26187363a0d0e1d39df7ed042e7a187eb17cac3d
quentindanneville <quentin.danneville@gmail.com>
2022-02-05 16:31:51 +0100

    [CI/CD] Docker nextjs & nginx (#2)

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_SERVER:/app << 'ENDSSH'
+    cd /app
+    docker login registry.gitlab.com -u $USERNAME -p $TOKEN
+    docker pull registry.gitlab.com/pote.dev/website:nextjs
+    docker pull registry.gitlab.com/pote.dev/website:nginx
+    docker-compose -f docker-compose.yml up -d --build
+ENDSSH
