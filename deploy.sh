079ed2946697bb761c3e350bbe98e696adaefa4c
quentindanneville <quentin.danneville@gmail.com>
2022-02-10 17:56:21 +0100

    [Devops] CI/CD with docker-compose (#17)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh $SSH_USER@$SSH_SERVER << 'ENDSSH'
-    cd /home/potedev/app
+    cd /home/potedev/app/website
     docker login registry.gitlab.com -u $USERNAME -p $TOKEN
     docker pull registry.gitlab.com/pote.dev/website:nextjs
     docker pull registry.gitlab.com/pote.dev/website:nginx
