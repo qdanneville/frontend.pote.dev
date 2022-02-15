20b73c4e373170b5be2f78449f752a3bc98a86e0
quentindanneville <quentin.danneville@gmail.com>
2022-02-15 19:07:33 +0100

    Dockerfile WIP

---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export const config = {
+    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:4000"
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { config } from '../config'
+
+console.log('config :', config);
 
 const instance = axios.create({
-    baseURL: "http://192.168.1.101:3000"
+    baseURL: config.API_BASE_URL
 })
 
 export default instance
