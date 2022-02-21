2b258fd6c5688ee48147f5453efc4421a66a7f07
quentindanneville <quentin.danneville@gmail.com>
2022-02-21 23:19:13 +0100

    [Devops] done

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ssh $SSH_USER@$SSH_SERVER << 'ENDSSH'
-    cd /home/potedev/app/website
-    docker login registry.gitlab.com -u $USERNAME -p $TOKEN
-    docker pull registry.gitlab.com/pote.dev/website:nextjs
-    docker pull registry.gitlab.com/pote.dev/website:nginx
-    docker-compose -f docker-compose.yml up -d --build
-ENDSSH
